f70d970c864afc8d7ee1232ae4fc9c7c9c531d94
JeloRebaya <227338999+JeloRebaya@users.noreply.github.com>
2025-11-12 19:44:27 +0800

    gagfasg

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('blog', function () {
+    return view('blog');
+});
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('portfolio', function () {
+    return view('portfolio');
+});
+
+Route::get('welcome', function () {
     return view('welcome');
 });
