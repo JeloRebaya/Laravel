7aab7a6327b7438a3e8efbcbb891a8ce48089b53
JeloRebaya <jrebaya23-0150@cca.edu.ph>
2025-11-23 15:38:52 +0800

    updafezzz

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,12 +7,19 @@
     
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
+
 <body>
     <header class="hero">
-        <div class="container">
-            <h1>Hello, I'm <span>Jelo Rebaya</span></h1>
-            <p>I'm a 3rd YR BSCS Student</p>
-            <a href="#projects" class="btn">View My Work</a>
+        <div class="container hero-container">
+            <div class="intro">
+                <h1>Hello, I'm <span>Jelo Rebaya</span></h1>
+                <p>I'm a 3rd YR BSCS Student</p>
+                <a href="#projects" class="btn">View My Work</a>
+            </div>
+
+            <div class="image-placeholder">
+                <img src="https://i.pinimg.com/474x/16/57/b5/1657b55d2e304e0c5e422216dc831dc0.jpg" alt="Profile Picture">
+            </div>
         </div>
     </header>
 
@@ -20,7 +27,7 @@
         <div class="container">
             <h2>About Me</h2>
             <p>
-				Idol ko si Spiderman.
+                Idol ko si Spiderman.
             </p>
         </div>
     </section>
@@ -28,7 +35,7 @@
     <section id="contact" class="section">
         <div class="container">
             <h2>Contact Me</h2>
-            <p>You can reach me at <a href="mailto:your.email@example.com">jrebaya23-0150@cca.edu.ph</a></p>
+            <p>You can reach me at <a href="mailto:jrebaya23-0150@cca.edu.ph">jrebaya23-0150@cca.edu.ph</a></p>
         </div>
     </section>
 
